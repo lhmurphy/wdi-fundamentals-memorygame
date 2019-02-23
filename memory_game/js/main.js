43304fdb20aa3116e73bc13b3cd99dfa09683196
lhmurphy <38182323+lhmurphy@users.noreply.github.com>
2019-02-23 10:51:25 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,23 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = ["queen", "queen", "king", "king"]; // the four cards that we have named
 var cardsInPlay = []; //this will hold the cards that are in play
 
-var cardOne = cards[0]; //represents the first card that the user flipped
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 	} else {alert("Sorry, try again")
-}};
+}}
+}
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
+// got stuck for ages on the 1st function
+
