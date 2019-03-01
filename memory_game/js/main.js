918ed937c25193c0b9733e6f51cb783b9f653c53
lhmurphy <38182323+lhmurphy@users.noreply.github.com>
2019-03-01 11:02:30 +0000

    final game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,65 @@
-var cards = ["queen", "queen", "king", "king"]; // the four cards that we have named
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+]; // the four cards that we have named
+
 var cardsInPlay = []; //this will hold the cards that are in play
 
 var checkForMatch = function() {
 	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
-	} else {alert("Sorry, try again")
+	} else {alert("Sorry, try again"); 
 }}
-}
+};
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-	checkForMatch();
-}
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch();	
+	this.setAttribute('src', cards[cardId].cardImage);
+};
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+/* create button to reset game
+var resetGame = document.getAttributeByClassName('button');
+resetGame.
+// if cards 
+*/
+
+createBoard();
 
 
-// got stuck for ages on the 1st function
+
+// got stuck for ages on the flipCard function - I needed to call the function at the bottom! 
 
